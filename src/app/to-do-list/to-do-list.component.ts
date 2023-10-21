5599c23a4eaa87b951aa45fa6e509c460e2fb672
Pamod0 <114252757+Pamod0@users.noreply.github.com>
2023-10-21 22:13:56 +0530

    project added

---
+++ src/app/to-do-list/to-do-list.component.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-to-do-list',
+  templateUrl: './to-do-list.component.html',
+  styleUrls: ['./to-do-list.component.scss']
+})
+export class ToDoListComponent {
+  taskArray = [{ taskName: 'Brush teeth', isCompleted: false }];
+
+  onSubmit(form: NgForm) {
+    console.log(form);
+
+    this.taskArray.push({
+      taskName: form.controls['task'].value,
+      isCompleted: false
+    })
+
+    form.reset();
+  }
+
+  onDelete(index: number) {
+    console.log(index);
+
+    this.taskArray.splice(index, 1);
+  }
+
+  onCheck(index: number) {
+    console.log(this.taskArray);
+
+    this.taskArray[index].isCompleted = !this.taskArray[index].isCompleted;
+  }
+}
