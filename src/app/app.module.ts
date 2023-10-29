47412ce767209f7ac6e10271975fe6504ed5422a
Pamod0 <114252757+Pamod0@users.noreply.github.com>
2023-10-29 10:43:48 +0530

    login page added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ToDoListComponent } from './to-do-list/to-do-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    ToDoListComponent
+    AppComponent
   ],
   imports: [
     FormsModule,
