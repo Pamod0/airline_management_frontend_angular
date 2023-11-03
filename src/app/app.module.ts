97a475a8a90d9a0bfc35470261962b824980b97e
Pamod0 <114252757+Pamod0@users.noreply.github.com>
2023-11-03 11:18:14 +0530

    essential components added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginPageComponent } from './login-page/login-page.component';
-import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
-import { RegisterPageComponent } from './register-page/register-page.component';
+import { LoginPageComponent } from './user/login-page/login-page.component';
+import { ForgotPasswordComponent } from './user/forgot-password/forgot-password.component';
+import { RegisterPageComponent } from './user/register-page/register-page.component';
 import { UsersListComponent } from './user-management/users-list/users-list.component';
 import { AddEditUsersComponent } from './user-management/add-edit-users/add-edit-users.component';
+import { UserHomeComponent } from './user/user-home/user-home.component';
+import { MainPageComponent } from './main-page/main-page.component';
 
 
 @NgModule({
@@ -19,7 +21,9 @@ import { AddEditUsersComponent } from './user-management/add-edit-users/add-edit
     ForgotPasswordComponent,
     RegisterPageComponent,
     UsersListComponent,
-    AddEditUsersComponent
+    AddEditUsersComponent,
+    UserHomeComponent,
+    MainPageComponent
   ],
   imports: [
     FormsModule,
